d6d2b456d22e382e0bdc7f7201dc53d2352c5d50
Ahsan Zaheer <130145473+Ahsan-Zaheer@users.noreply.github.com>
2025-06-30 17:10:02 +0500

    Add book summary creation endpoint and integrate

--- utils/api.ts
+++ utils/api.ts
@@ -17,6 +17,19 @@ export const askQuestion = async (question) => {
 
 }
 
+export const createBook = async (summary) => {
+    const res = await fetch(new Request(createUrl('/api/book/summary'), {
+        method: 'POST',
+        body: JSON.stringify({ summary }),
+    }));
+    if(res.ok) {
+        const data = await res.json();
+        return data.data;
+    } else {
+        throw new Error('Failed to create book');
+    }
+}
+
 
 export const saveTitle = async (bookId, title) => {
     const res = await fetch(new Request(createUrl('/api/book/title'), {
